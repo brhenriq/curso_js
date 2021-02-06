5df6d7aa9b08a18ae4e7628671bd919120c66e1b
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-02-05 21:45:27 -0300

    Second exercise: browser functions

---
+++ 02_navegador/script.js
@@ -0,0 +1,6 @@
+const num1 = prompt('Informe um númeropara soma:');
+const num2 = prompt('Informe outro número para soma:');
+
+const resultado = Number(num1) + Number(num2);
+
+alert(`O resultado da soma é: ${resultado}`);

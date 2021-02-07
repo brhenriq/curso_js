a9c32181b2c5bc6e76de6a77a53678ee56785254
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-02-06 22:01:14 -0300

    refactor: Fourth exercise

--- 04_strings/script.js
+++ 04_strings/script.js
@@ -2,7 +2,7 @@ const nome = prompt('Digite seu nome completo:');
 
 document.body.innerHTML += `Seu nome é: ${nome} <br />`;
 document.body.innerHTML += `Seu nome tem: ${nome.length} letras <br />`;
-document.body.innerHTML += `A segunda Letra do seu nome é: ${nome.slice(1,2)} <br />`;
+document.body.innerHTML += `A segunda Letra do seu nome é: ${nome[1]} <br />`;
 document.body.innerHTML += `O primeiro índice da letra "a" é: ${nome.indexOf('a')} <br />`;
 document.body.innerHTML += `O último índice da letra "a" é ${nome.lastIndexOf('a')} <br />`;
 document.body.innerHTML += `As 3 últimas letras do seu nome são: ${nome.slice(-3)} <br />`;

8639b5a0ebcef9892ea4d477c779bd0626d699d2
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-02-15 15:01:11 -0300

    tenth exercise: Font color to white

--- 10_nodeList/assets/js/main.js
+++ 10_nodeList/assets/js/main.js
@@ -5,4 +5,5 @@ const backgroundColorBody = getComputedStyle(document.body).backgroundColor;
 
 for (const paragraph of paragraphs) {
   paragraph.style.backgroundColor = backgroundColorBody;
+  paragraph.style.color = '#FFF';
 }

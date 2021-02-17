0d0ef4a94dca81e0c760774277838bc55264e5ab
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-02-17 18:26:07 -0300

    refactor: logic 03

--- 11_logica/ex_03.js
+++ 11_logica/ex_03.js
@@ -1,8 +1,8 @@
 function fizzBuzz(num) {
-  if (Number(num)) {
-    if (!(num % 5) && !(num % 3)) return "FizzBuzz";
-    if (!(num % 5)) return "Buzz";
-    if (!(num % 3)) return "Fizz";
+  if (typeof num !== 'Number') {
+    if (num % 5 === 0 && num % 3 === 0) return 'FizzBuzz';
+    if (num % 5 === 0) return 'Buzz';
+    if (num % 3 === 0) return 'Fizz';
   }
   return num;
 }

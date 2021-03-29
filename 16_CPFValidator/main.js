79d81f401d0cb90b6f9e5831aaaacdc4dc2c9f30
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-03-29 16:53:13 -0300

    16 exercise: Refactor code

--- 16_CPFValidator/main.js
+++ 16_CPFValidator/main.js
@@ -1,44 +1,45 @@
-let cpf = '705.484.450-52';
-
-function clearCpf(cpf) {
-  return cpf.replace(/\D+/g, '');
-}
-
-function arrayCpf(cpf) {
-  return Array.from(cpf);
-}
-
-function cpfSizeValidation(size) {
-  return size !== 11;
-}
-
-function cpfValidation(cpfNumbers, cpfDigits, cont = 10) {
-  if (cpfSizeValidation(cpfDigits.length + cpfNumbers.length)) return 'CPF Inválido';
-
-  let ac = 0;
-
-  cpfNumbers.forEach(el => {
-    ac += el * cont
-    cont--;
+function ValidaCPF(cpf) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+      return cpf.replace(/\D+/g, '');
+    }
   });
-
-  let digit = cpfDigits.shift();
-
-  cpfNumbers.push(digit);
-  
-  let res = (11-(ac%11)) > 9 ? 0 : (11-(ac%11));
-
-  if (Number(digit) !== res)  return 'CPF Inválido';
-  
-  if(cpfDigits) cpfValidation(cpfNumbers, cpfDigits, 11);
-
-  return 'CPF válido';
 }
 
-cpf = arrayCpf(clearCpf(cpf));
+ValidaCPF.prototype.valida = function() {
+  if(typeof this.cpfLimpo === 'undefined') return false;
+  if(this.cpfLimpo.length !== 11) return false;
+  if(this.isSequencia()) return false;
 
-let cpfNumbers = cpf.slice(0, 9);
-let cpfDigits = cpf.slice(9, 11);
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+  
+  const novoCpf = cpfParcial + digito1 + digito2;
 
-const result = cpfValidation(cpfNumbers, cpfDigits);
-console.log(result);
+  return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+  let reg = cpfArray.length +1;
+
+  const total = cpfArray.reduce((ac, val) => {
+    ac += (reg * Number(val));
+    reg--;
+    return ac;
+  }, 0);
+
+  const digito = 11 - (total % 11);
+  return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+  return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo;
+}
+
+const cpf = new ValidaCPF('070.987.720-03');
+
+if(cpf.valida()) console.log('CPF Válido');
+else console.log('CPF Inválido');
